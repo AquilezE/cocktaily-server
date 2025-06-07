644c49ef5f0459562f7b3d062024dfcc0f8bc986
Marco Antonio Varillas Olguin <zS22013628@estudiantes.uv.mx>
2025-06-07 11:41:45 -0600

    Se crean estadisticas

--- api/routes/index.js
+++ api/routes/index.js
@@ -12,6 +12,8 @@ router.use('/v1/upload', require('./upload'));
 router.use('/v1/likes', require('./like'));
 router.use('/v1/devices', require('./devices'));
 router.use('/v1/verification', require('./vetification'));
+router.use('/v1/stats', require('./stats'));
+
 
 
 module.exports = router;
